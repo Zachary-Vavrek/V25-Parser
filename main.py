9a798d7c95001c9aa2b2b150f6bbb888869bc4af
Zachary-Vavrek <zachary.j.vavrek@gmail.com>
2023-09-25 19:44:24 -0700

    Reading .odt

--- main.py
+++ main.py
@@ -16,24 +16,6 @@
 
 # Strong recommendation here to use the `with` keyword.
 
-# def main():
-#     with open('test.txt', encoding="utf-8") as f:
-#         test_read = f.read()
-#     print(test_read)
-#     # I'm not fond of the string.method.method thing going on here.
-#     # It's just a lot of function composition happening on one line.
-#     # (Which is fine.)
-#     # First step is a find-replace to convert double blank lines to singles.
-#     # Second splits based on double blank lines.
-#     test_split = test_read.replace('\n\n\n','\n\n').split('\n\n')
-#     # Looking at documentation, the method splitlines is interesting.
-#     print(len(test_split))
-#     for x in test_split:
-#         print("---")
-#         print(x)
-#         print("---")
-
-# main()
 
 # Condensed version of reading:
 # Use a with to handle the initial reading step, as in:
@@ -49,29 +31,58 @@
 
 # Now to write something.
 
-from os.path import exists
+# from os.path import exists
+
+# def main():
+#     # I am immediately concerned about "how do I make a new file but stop
+#     # if the file already exists?" because the documentation I was looking at
+#     # didn't detail that situation.  The open() documentation does, though!
+#     # with open('write_test.txt', 'x', encoding="utf-8") as f:
+#     #     f.write("Test: Write?")
+#     # This should work the first time and fail somehow the second time.
+#     # Learn by doing.
+#     # Alright, that does what it says on the tin.  Commenting out.
+
+#     # There should be a better way of handling it when there's already a file.
+#     # Print a complain to the screen, but not in a 'oh no the program failed'
+#     # way.  I want some way of checking if the output is there already.
+#     # This would allow for prompting "overwrite? y/n" for instance.
+#     with open('test.txt', 'r', encoding="utf-8") as f:
+#         test_read = f.read()
+#     if exists('write_test.txt'):
+#         print("Output file already present, stopping.")
+#         returns
+#     test_split = test_read.replace('\n\n\n','\n\n').split('\n\n')
+#     with open('write_test.txt', 'a', encoding="utf-8") as f:
+#         f.write(test_split[1])
+
+# What happens if I just use regular open() on an odt file?
+# Learn by doing.
+# Answer: utf-8 encoding chokes.
+
+# I find the status of dealing with ODF files enraging.
+# I generally find it enraging any time there are details left unexplained.
+
+# Using odfpy.  Some headache about learning what's going on with venv stuff.
+# This pulls out each paragraph and reconstructs structure, though.
+
+from odf import text, teletype
+from odf.opendocument import load
+
+# Why the .opendocument?
 
 def main():
-    # I am immediately concerned about "how do I make a new file but stop
-    # if the file already exists?" because the documentation I was looking at
-    # didn't detail that situation.  The open() documentation does, though!
-    # with open('write_test.txt', 'x', encoding="utf-8") as f:
-    #     f.write("Test: Write?")
-    # This should work the first time and fail somehow the second time.
-    # Learn by doing.
-    # Alright, that does what it says on the tin.  Commenting out.
-
-    # There should be a better way of handling it when there's already a file.
-    # Print a complain to the screen, but not in a 'oh no the program failed'
-    # way.  I want some way of checking if the output is there already.
-    # This would allow for prompting "overwrite? y/n" for instance.
-    with open('test.txt', 'r', encoding="utf-8") as f:
-        test_read = f.read()
-    if exists('write_test.txt'):
-        print("Output file already present, stopping.")
-        return
-    test_split = test_read.replace('\n\n\n','\n\n').split('\n\n')
-    with open('write_test.txt', 'a', encoding="utf-8") as f:
-        f.write(test_split[1])
-
+    doc = load("Test doc.odt")
+    print("holding")
+    paras = doc.getElementsByType(text.P)
+    print(paras)
+    print("holding")
+    holding = []
+    for para in paras:
+        holding.append(teletype.extractText(para))
+    print("holding")
+    print(holding)
+    thing = "\n".join(holding)
+    print(thing)
+    print("holding")
 main()
